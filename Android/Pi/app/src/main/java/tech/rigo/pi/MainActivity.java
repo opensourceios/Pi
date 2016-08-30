07b628b9ddde43d9dcdfa63dce2d1fb243d3ce6d
Rigoberto Molina <rigothedev@gmail.com>
2016-08-29 19:03:14 -0700

    [android] Menu items + NavBar title change

--- Android/Pi/app/src/main/java/tech/rigo/pi/MainActivity.java
+++ Android/Pi/app/src/main/java/tech/rigo/pi/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_dashboard) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_subjects) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_favorites) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_study) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_log_out) {
 
         }
 

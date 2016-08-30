0b7c276282f6830b5ace92521c75c38ebf5e003d
Rigoberto Molina <rigothedev@gmail.com>
2016-08-30 07:04:42 -0700

    Clean fragment code

--- Android/Pi/app/src/main/java/layout/SubjectsFragment.java
+++ Android/Pi/app/src/main/java/layout/SubjectsFragment.java
@@ -1,7 +1,5 @@
 package layout;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,14 +8,6 @@ import android.view.ViewGroup;
 
 import tech.rigo.pi.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SubjectsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SubjectsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SubjectsFragment extends Fragment {
     public SubjectsFragment() {
         // Required empty public constructor

399247db645c37ec5a3649d59008082c89285685
Rigoberto Molina <rigothedev@gmail.com>
2016-08-29 18:02:01 -0700

    [android] Login + Sign up + Main + Side menu

---
+++ Android/Pi/app/src/main/java/tech/rigo/pi/LoginActivity.java
@@ -0,0 +1,37 @@
+package tech.rigo.pi;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    
+    Button loginButton;
+    EditText usernameTextField;
+    EditText passwordTextField;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        this.loginButton = (Button) findViewById(R.id.loginButton);
+        this.usernameTextField = (EditText) findViewById(R.id.usernameTextField);
+        this.passwordTextField = (EditText) findViewById(R.id.passwordTextField);
+    }
+    
+    public void login(View v) {
+        Intent MainActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(MainActivityIntent);
+    }
+    
+    public void signUpInstead(View v) {
+        Intent SUIIntent = new Intent(this, SignUpActivity.class);
+        startActivity(SUIIntent);
+    }
+}
---
+++ Android/Pi/app/src/main/java/tech/rigo/pi/SignUpActivity.java
@@ -0,0 +1,22 @@
+package tech.rigo.pi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+    
+    public void signUp(View v) {
+        
+    }
+    
+    public void loginInstead(View v) {
+        
+    }
+}
